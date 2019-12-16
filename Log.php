548e9b9fb0009f6a2c3b3b6ebf2ad6283322a8d3
orionic969 <daniil.shadow@mail.ru>
2019-12-16 17:19:57 +0300

    1.6

--- Log.php
+++ Log.php
@@ -13,6 +13,8 @@ class Log extends core\LogAbstract implements core\LogInterface
                 if (file_put_contents($dir,date(DATE_COOKIE)."\r\n".$str))
                     self::Instance()->log[]=$str;
                 else throw new RzekaMansur_Exception("Error adding message");
+				vers = $(git symbolic-ref --short -q HEAD>version);
+				echo "\r\n Версия программы " >> ./log/version
             }
             else {
                 mkdir(__DIR__."/../../../log");
